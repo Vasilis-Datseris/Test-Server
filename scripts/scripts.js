ad7604faf1dde92329fac529536acac040f6139a
Vasilis Datseris <iekdatseris@gmail.com>
2023-10-07 21:19:00 +0300

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,13 @@ const body = document.getElementsByTagName('Body')[0];
 
 const scripts = [
     //"https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js",
+    "scripts/Brotli.js",
     "scripts/Lottie-Player.min.js",
     "scripts/Lottie.js",
     "scripts/Waves.js",
     "scripts/Flappy.js",
     "scripts/splashscreen.js",
     "_content/MudBlazor/MudBlazor.min.js",
-    "scripts/Brotli.js",
 ];
 scripts.forEach((script) => {
     let register = document.createElement("script");
