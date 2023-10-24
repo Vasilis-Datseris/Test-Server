d93976ef9c0d11c06043aabb6d7c585a9c644391
Vasilis Datseris <iekdatseris@gmail.com>
2023-10-25 01:39:44 +0300

    .

---
+++ scripts/Lottie.js
@@ -0,0 +1,42 @@
+﻿let InstallPrompt = null;
+
+LottiePlay = (container, loop = true) => {
+    let lottie = container.getLottie();
+    lottie.loop = loop;
+    lottie.play();
+}
+LottiePlaySegments = (container, Segments = [0, 0], loop = true) => {
+    let lottie = container.getLottie();
+    const max = lottie.totalFrames;
+    if (Segments[0] === 0 && Segments[1] === 0)
+        Segments[1] = max;
+    lottie.loop = loop;
+    lottie.playSegments(Segments, !loop);
+}
+OnRender = (container) => {
+    let lottie = container.getLottie();
+    container.appendChild(lottie.wrapper.parentElement);
+}
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    InstallPrompt = e;
+
+    InstallPrompt.userChoice.then(function (choiceResult) {
+        //console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+
+        //try {
+        //    DotNet.invokeMethod('Portfolio_Blazor.Client', 'Download_Toast', choiceResultmax-width: {(_isPhone ? "100%" : _isTablet ? "75%" : "50%")}       return Result.outcome === 'accepted';
+    });
+})
+
+installPortfolio = async () => {
+    if (InstallPrompt !== null) {
+        let result = await InstallPrompt.prompt();
+        return result.outcome === 'accepted';
+    } 
+}
+initialization = async () => {
+    let main = document.getElementsByClassName('main-container')[0];
+    main.addEventListener('touchmove', function () { e.preventDefault(); }, { passive: false });
+}
